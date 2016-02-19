fe24441aa9e81d23c9b9700c779d9991e073994b
whx4J8 <750702272@qq.com>
2016-02-19 22:22:06 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+elastic-job的简单使用demo
+
+后续更新elatic-job的高级使用方法
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,6 @@ import com.dangdang.ddframe.reg.base.CoordinatorRegistryCenter;
 import com.dangdang.ddframe.reg.zookeeper.ZookeeperConfiguration;
 import com.dangdang.ddframe.reg.zookeeper.ZookeeperRegistryCenter;
 import job.DemoJob;
-import job.DemoJob2;
-
-import java.io.Serializable;
 
 /**
  * Created by wanghongxing on 16/2/17.
@@ -29,14 +26,11 @@ public class Main{
         CoordinatorRegistryCenter regCenter = new ZookeeperRegistryCenter(zkConf);//注册中心
 
         JobConfiguration jobConfig = new JobConfiguration("simpleJob", DemoJob.class, 1, "0/5 * * * * ?");//配置job
-        //jobConfig.setOverwrite(true);// 将本地的任务更新到zk上,正在跑的任务也会被立即更新
-        JobConfiguration jobConfig2 = new JobConfiguration("simpleJob2", DemoJob2.class, 1, "0/10 * * * * ?");//配置job
-        jobConfig.setOverwrite(Boolean.TRUE);
+        jobConfig.setOverwrite(true);// 将本地的任务更新到zk上,正在跑的任务也会被立即更新
 
         regCenter.init();//注册中心启动
 
         new JobScheduler(regCenter,jobConfig).init();//任务启动
-        new JobScheduler(regCenter,jobConfig2).init();//任务启动
 
         System.out.println("init complete.");
     }
